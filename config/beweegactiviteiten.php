14d484f4fcf39f8b0b1f3897fdce0113cf0ff573
Kay Hardam <hardamkay@gmail.com>
2026-05-16 09:28:40 +0200

    feat(tools): add beweegactiviteiten config for leerdoel-coach

---
+++ config/beweegactiviteiten.php
@@ -0,0 +1,39 @@
+<?php
+
+return [
+    'activiteiten' => [
+        'springen_kast' => [
+            'naam' => 'Springen over een kast',
+            'werkwoorden' => ['afzetten', 'landen', 'hurken', 'spreiden', 'doorbuigen'],
+        ],
+        'vangen' => [
+            'naam' => 'Vangen',
+            'werkwoorden' => ['vangen', 'ontvangen', 'meebuigen', 'positioneren', 'richten'],
+        ],
+        'balanceren' => [
+            'naam' => 'Balanceren',
+            'werkwoorden' => ['handhaven', 'herstellen', 'lopen', 'staan', 'omdraaien'],
+        ],
+        'mikken' => [
+            'naam' => 'Mikken en werpen',
+            'werkwoorden' => ['mikken', 'werpen', 'raken', 'richten', 'volgen'],
+        ],
+        'klimmen' => [
+            'naam' => 'Touwklimmen',
+            'werkwoorden' => ['klimmen', 'klemmen', 'afdalen', 'vastpakken', 'opdrukken'],
+        ],
+    ],
+
+    'niet_waarneembaar' => [
+        'weten',
+        'begrijpen',
+        'kennen',
+        'voelen',
+        'samenwerken',
+        'leren',
+        'ervaren',
+        'beleven',
+        'plezier hebben',
+        'inzien',
+    ],
+];
