4bdb0e126653be0bafdc9c52c602d305c05bf535
Kay Hardam <hardamkay@gmail.com>
2026-05-22 14:04:59 +0200

    chore(tools): verwijder dode promt-generator code

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,11 +1,5 @@
-import { initPromptGenerator } from "./promt-generator";
-
-initPromptGenerator();
-
 import Alpine from "alpinejs";
 import { leerdoelCoach } from "./leerdoel-coach";
-
 Alpine.data("leerdoelCoach", leerdoelCoach);
-
 window.Alpine = Alpine;
 Alpine.start();
