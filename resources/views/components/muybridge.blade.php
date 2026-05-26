ed3e7d009ceb205adc29e1039e46a4dc131d31b6
Kay Hardam <hardamkay@gmail.com>
2026-05-26 14:57:24 +0200

    feat(muybridge): scaffold anonymous blade component

---
+++ resources/views/components/muybridge.blade.php
@@ -0,0 +1,9 @@
+@props([
+'color' => '#0f0f0f',
+])
+
+<div class="muybridge" style="--muybridge-color: {{ $color }};">
+    <div class="muybridge__strip">
+        {{-- 4 walk-cycle frames volgen in stap 3 --}}
+    </div>
+</div>
